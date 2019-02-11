0ac5209ef39c60186262016d5d359d2eaab028fe
JoshuaViera <joshuaviera95@gmail.com>
2019-02-11 18:17:40 -0500

    Used a UIButton and UIView.transition() method to toggle between 2 images

--- DuckSizer/ViewController.swift
+++ DuckSizer/ViewController.swift
@@ -10,18 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var cat: UIImageView!
+    @IBOutlet weak var cat: UIButton!
+    @IBOutlet weak var duck: UIImageView!
+    
     private var animator: UIViewPropertyAnimator!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         animator = UIViewPropertyAnimator(duration: 1.0, curve: .easeInOut, animations: {
-            self.cat.transform = CGAffineTransform(scaleX: 10.0, y: 10.0)
+            self.duck.transform = CGAffineTransform(scaleX: 10.0, y: 10.0)
         })
     }
 
     @IBAction func sliderChanged(_ sender: UISlider) {
         animator.fractionComplete = CGFloat(sender.value)
+    }
+    
+    @IBAction func imageTransition(_ sender: UIButton) {
+        
+        if cat.imageView?.image == UIImage(named: "cat"){
+            UIView.transition(with: cat, duration: 1.0, options: [.transitionFlipFromRight], animations: {
+                self.cat.setImage(UIImage(named: "duck"), for: .normal)
+            })
+        } else {
+            UIView.transition(with: cat, duration: 1.0, options: [.transitionFlipFromRight], animations: {
+                self.cat.setImage(UIImage(named: "cat"), for: .normal)
+            })
+        }
     }
 }
 
